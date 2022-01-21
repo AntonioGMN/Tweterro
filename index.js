2700712d8176669ffe654535de36a09ce16e4706
Antonio <antonio.garciamn@gmail.com>
2022-01-21 14:59:42 -0300

    post sign-up

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from "express";
+import cors from "cors";
 
 const serve = express();
+serve.use(cors());
+serve.use(express.json());
 
-const usuario = {
-	username: null,
-	avatar: null,
-};
+let usuario = {};
+let tweets = [];
 
-//serve.post("/sign-up", (req, res) => {});
+serve.post("/sign-up", (req, res) => {
+	console.log(req.body);
+	usuario = { ...req.body };
+	res.send("OK");
+});
+
+serve.get("/tweets", (req, res) => {
+	res.send(tweets);
+});
+
+serve.post("/tweets", (req, res) => {
+	tweets.push(req.body);
+	res.send("OK");
+});
+
+serve.get("/", (req, res) => {
+	res.send(usuario);
+});
 
 serve.listen(5000, () => {
 	console.log("escutando");
